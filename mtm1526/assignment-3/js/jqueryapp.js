3487e7edb88feae54acfc6b11786d2254e3464f4
grim0035 <sgrimes@rogers.com>
2012-05-25 11:14:08 -0400

    setup files created

---
+++ mtm1526/assignment-3/js/jqueryapp.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	$('form').on('submit', function (ev) {
+	// Stops elements from using the POST request or doing anything as it normally does.
+	ev.preventDefault();
+	//console.log('form submitted'); //test if this is working
+	
+	
+	});
+	
+	
+});

de3fa6a54c9dfa32abc9c522f30e472c39db9dc7
grim0035 <sgrimes@rogers.com>
2012-05-25 12:06:59 -0400

    started to target div

--- mtm1526/assignment-3/js/jqueryapp.js
+++ mtm1526/assignment-3/js/jqueryapp.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 	// Stops elements from using the POST request or doing anything as it normally does.
 	ev.preventDefault();
 	//console.log('form submitted'); //test if this is working
-	
+	var color = $('#color').val(); //same as item.value
+	$('div').css('background-color'), ('.val');
 	
 	});
 	

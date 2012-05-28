956c4158f4f0f5021377377bd71d1960e144a6a8
Scott Grimes <sgrimes@rogers.com>
2012-05-27 21:13:48 -0400

    created index.php

---
+++ mtm1531/assignment-3/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Assignment 3 &middot; Form Validation</title>
+</head>
+
+<body>
+
+	<form method="post" action="index.php">
+	
+	
+	
+	
+	
+	</form>
+	
+
+
+</body>
+</html>

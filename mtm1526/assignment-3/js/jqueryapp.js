a2645fc227ca765c70f7a58615b65a01827473f6
Scott Grimes <sgrimes@rogers.com>
2012-05-30 01:00:18 -0400

    trying to get change working

--- mtm1526/assignment-3/js/jqueryapp.js
+++ mtm1526/assignment-3/js/jqueryapp.js
@@ -5,11 +5,27 @@ $(document).ready(function() {
 	// Stops elements from using the POST request or doing anything as it normally does.
 	ev.preventDefault();
 	//console.log('form submitted'); //test if this is working
-	var color = $('#color').val(); //same as item.value
+	var color = $('#color').val();
+	//var property = $('#property').val();
+	var brdrcolor = $('#color').val();
+	var bgcolor = $('#color').val();
+	var txtcolor = $('#color').val();//same as item.value
 	//console.log(color); //test if color value is working
-	 //$('#circ').css('background-color','#fff'); this works
-	 $('#circ').css('background-color', color); //need to use value of 'color'
-	 //need a switch loop for the dropdown to use value of 'color'
+	//$('#circ').css('background-color','#fff'); this works
+	//$('#circ').css('background-color', color); //this works.
+	//need a switch loop for the dropdown to use value of 'color'
+		/*if ($(property) == ('#background-color')) {
+			console.log('yay!');
+			//$('#circ').css('background-color', bgcolor);
+		}*/
+		$('#property').change(function () {
+			var property = "";
+			$("select option:selected").each(function () {
+			property = $('#color').val();
+			});
+		})
+		.change();
+		console.log('yay!'); //this works
 
 	});
 	

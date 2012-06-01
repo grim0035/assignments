fed85337fe0949f44a60f4bffcbc20a2cad0dd05
Scott Grimes <sgrimes@rogers.com>
2012-06-01 01:02:13 -0400

    hide part of button works

--- mtm1526/assignment-3/js/jqueryapp.js
+++ mtm1526/assignment-3/js/jqueryapp.js
@@ -1,51 +1,55 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	$('form').on('submit', function (ev) {
+	$('#apply').click(function (ev) {
 	// Stops elements from using the POST request or doing anything as it normally does.
 	ev.preventDefault();
 	//console.log('form submitted'); //test if this is working
 	var color = $('#color').val();
-	//var property = $('#property').val();
-	var brdrcolor = $('border-color').val();
-	var bgcolor = $('background-color').val();
-	var txtcolor = $('text-color').val();//same as item.value
+	var property = $('#property').val(); //not needed?
+	//var brdrcolor = $('border-color').val(); //not needed?
+	//var bgcolor = $('background-color').val(); //not needed?
+	//var txtcolor = $('text-color').val();//same as item.value
 	//$('#circ').css('background-color', color); //this works.
 	//need a switch loop for the dropdown to use value of 'color'
-		/*if ($(property) == ('#background-color')) {
-			console.log('yay!');
-			//$('#circ').css('background-color', bgcolor);
-		}*/
+	
 		$('#property').change(function () {
 			var property = "";
 			$("select option:selected").each(function () {
-			//property == $('#property').val();
-			$('#circ').css('background-color', color); //this works.
-
-			});
-		})
-		
-		$('#property').change(function () {
-			var property = "";
-			$("select option:selected").each(function () {
-			//property == $(bgcolor).val();
+			//property == $(border-color).val();
 			$('#circ').css('border-color', color); 
 
 			});
 		})
+		$('#property').change(function () {
+			var property = "";
+			$("select option:selected").each(function () {
+			//property == $(background-color).val();
+				$('#circ').css('background-color', color); 
+
+			});
+		})
+		
 		
 		$('#property').change(function () {
 			var property = "";
 			$("select option:selected").each(function () {
-			//property == $(bgcolor).val();
+			//property == $(color).val();
 			$('#circ').css('color', color); 
 
 			});
 		})
+		.change();
 		  //alert('Handler for .change() called.');
 		//console.log(property); //this works?
 
 	});
 	
+	$('#hideshow').click(function (ev) {
+		ev.preventDefault();
+
+	 $("#circ").hide(); //need if statement to hide/show #circ
+	
+	});
 	
 });

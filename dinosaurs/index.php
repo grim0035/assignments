e7d543c1b3ddae08135bd4fc834ccfd7712e19db
grim0035 <sgrimes@rogers.com>
2012-06-04 14:48:32 -0400

    added php code to display and delete dinosaurs

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -1,3 +1,18 @@
+<?php
+require_once 'includes/db.php';
+//pointer to db
+$sql = $db->query('
+	SELECT id, dino_name, loves_meat, in_jurassic_park
+	FROM dinosaurs
+	ORDER BY dino_name ASC
+');
+
+$results = $sql->fetchAll(); //gets data from db
+
+//Display the last error created by our database
+//var_dump($db->errorInfo());
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -5,15 +20,15 @@
 	<title>Dinosaurs</title>
 </head>
 <body>
-
-	<h2>Velociraptor</h2>
+	<?php foreach ($results as $dino) :?>
+	<h2><a href="single.php?id=<?php echo $dino['id']; ?>"><?php echo $dino['dino_name'];?></a></h2>
 	<dl>
 		<dt>Loves Meat</dt>
-		<dd>1</dd>
+		<dd><?php echo $dino['loves_meat'];?></dd>
 		<dt>In Jurassic Park</dt>
-		<dd>1</dd>
+		<dd><?php echo $dino['in_jurassic_park'];?></dd>
 	</dl>
-
+	<?php endforeach; ?>
 
 </body>
 </html>

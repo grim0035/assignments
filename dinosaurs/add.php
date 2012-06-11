81637d0efd8f7b0fcddb9bb14032fe2c99248490
grim0035 <sgrimes@rogers.com>
2012-06-11 12:42:00 -0400

    created form elements

--- dinosaurs/add.php
+++ dinosaurs/add.php
@@ -10,7 +10,23 @@ require_once 'includes/db.php';
 </head>
 
 <body>
-
-
+	<h1>Add a Dinosaur</h1>
+	<form method="post" action="add.php">
+		<div>
+			<label for"dino_name">Dinosaur Name</label>
+			<input id="dino_name" name="dino_name" required>
+		</div>
+		<fieldset>
+			<input type="radio" id="love" name="loves_meat" value="1">
+			<label for="love">Loves Meat</label>
+			<input type="radio" id="hate" name="loves_meat" value="0">
+			<label for="hate">Hates Meat</label>
+		</fieldset>
+		<div>
+			<input type="checkbox" id="in_jurassic_park" name="in_jurassic_park">
+			<label for="in_jurassic_park">In Jurassic Park Movie?</label>
+		</div>
+		
+		<button type="submit">Add Dinosaur</button>
 </body>
 </html>

20f6c857ff782dda0906ad449991123e21c44e81
grim0035 <sgrimes@rogers.com>
2012-06-18 10:35:53 -0400

    fixed edit page, missing id=get

--- mtm1531/assignment-6/old-edit.php
+++ mtm1531/assignment-6/old-edit.php
@@ -1,28 +1,30 @@
 <?php
+
 require_once 'includes/db.php';
 
 $errors = array();
-
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 $movie_title = filter_input(INPUT_POST, 'movie_title', FILTER_SANITIZE_STRING);
 $release_date = filter_input(INPUT_POST, 'release_date', FILTER_SANITIZE_STRING);
 $director = filter_input(INPUT_POST, 'director', FILTER_SANITIZE_STRING);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if (strlen($movie_title) < 1 || strlen($movie_title) > 256) {
+	if (strlen($movie_title) < 1  || strlen($movie_title) > 256) {
 		$errors['movie_title'] = true;	
 	}
 	
-	if (strlen($release_date) < 1 || strlen($release_date) > 256) {
+	if (strlen($release_date) < 4  || strlen($release_date) > 4) {
 		$errors['release_date'] = true;	
 	}
 	
-	if (strlen($director) < 1 || strlen($director) > 256) {
+	if (strlen($director) < 1  || strlen($director) > 256) {
 		$errors['director'] = true;	
 	}
+
 	
 	if (empty($errors)) {
 	// do DB stuff
+
 		$sql = $db->prepare('
 		UPDATE movies
 		SET movie_title = :movie_title
@@ -30,7 +32,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			, director = :director
 		WHERE id = :id
 		');
-		
 		$sql->bindValue(':id', $id, PDO::PARAM_INT);
 		$sql->bindValue(':movie_title', $movie_title, PDO::PARAM_STR);
 		$sql->bindValue(':release_date', $release_date, PDO::PARAM_STR);
@@ -41,11 +42,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		exit;
 	}
 } else {
-$sql = $db->prepare('
+	$sql = $db->prepare('
 	SELECT movie_title, release_date, director
 	FROM movies
 	WHERE id = :id
-');
+	
+	');
 		$sql->bindValue(':id', $id, PDO::PARAM_INT);
 		$sql->execute();
 		$results = $sql->fetch();
@@ -55,39 +57,35 @@ $sql = $db->prepare('
 		$director = $results['director'];
 
 }
-
-?>
-<!DOCTYPE HTML>
+?><!DOCTYPE HTML>
 <html>
 <head>
 <meta charset="utf-8">
-<title>edit <?php echo $results['movie_title'];?></title>
+<title>Edit </title>
 <link href="css/general.css" rel="stylesheet">
 
 </head>
 
 <body>
 	<p><a href="index.php">Back to the List</a></p>
+
 	<h1>Edit <?php echo $results['movie_title'];?></h1>
-
 	<form method="post" action="edit.php?id=<?php echo $id; ?>">
-
-		<div>
-			<label for"movie_title">Movie Title <?php if (isset($errors['movie_title'])) : ?> <strong class="error">is required</strong><?php endif; ?></label>
-			<input id="movie_title" name="movie_title" required value="<?php echo $results['movie_title'];?>">
+		<div class="form">
+			<label for"movie_title">Movie title <?php if (isset($errors['movie_title'])) : ?> <strong class="error">is required</strong><?php endif; ?></label>
+			<input id="movie_title" name="movie_title" required value="<?php echo $results['movie_title']; ?>">
 		</div>
+		<div class="form">
+			<label for="release_date">Year of Release <?php if (isset($errors['release_date'])) : ?> <strong class="error">is required</strong><?php endif; ?></label>
+			<input id="release_date" name="release_date" required value="<?php echo $results['release_date']; ?>">
 
-		<div>
-			<label for"release_date">Release Date <?php if (isset($errors['release_date'])) : ?> <strong class="error">is required</strong><?php endif; ?></label>
-			<input id="release_date" name="release_date" required value="<?php echo $results['release_date'];?>">
+		</div>
+		<div class="form">
+			<label for="director">Director <?php if (isset($errors['director'])) : ?> <strong class="error">is required</strong><?php endif; ?></label>
+			<input id="director" name="director" required value="<?php echo $results['director']; ?>">
 		</div>
 		
-		<div>
-			<label for"director">Director <?php if (isset($errors['director'])) : ?> <strong class="error">is required</strong><?php endif; ?></label>
-			<input id="director" name="director" required value="<?php echo $results['director'];?>">
-		</div>
-		
-		<button type="submit">Save</button>
+		<button type="submit">Edit</button>	
 	</form>
 </body>
 </html>

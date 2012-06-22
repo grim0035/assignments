330ecc28ce79a5ba96b9280197e0997ee5a16320
Scott Grimes <sgrimes@rogers.com>
2012-06-22 01:26:45 -0400

    tabs are loading need css current to work

--- mtm1526/assignment-4/js/general.js
+++ mtm1526/assignment-4/js/general.js
@@ -2,10 +2,31 @@
 
 $(document).ready(function () {
 	$.get('page-1.html', function (data) {
-	$('body').append(data);
+	$('#load-content').append(data);
 	});
 	
-	$('.current').on('click', function() {
-		$('.current').load('page-3.html');
+$('#tab-1').on('click', function(ev) {
+		ev.preventDefault();
+
+		$('#load-content').load('page-1.html');
 	});
+
+$('#tab-2').on('click', function(ev) {
+		ev.preventDefault();
+
+		$('#load-content').load('page-2.html');
+	});
+	
+$('#tab-3').on('click', function(ev) {
+		ev.preventDefault();
+
+		$('#load-content').load('page-3.html');
+	});
+	
+$('#tab-4').on('click', function(ev) {
+		ev.preventDefault();
+
+		$('#load-content').load('page-4.html');
+	});
+	
 });

a3423c290f64e7d196275ede19556a9c37603b5e
Scott Grimes <sgrimes@rogers.com>
2012-06-22 00:54:18 -0400

    fixed css

--- mtm1526/assignment-4/js/general.js
+++ mtm1526/assignment-4/js/general.js
@@ -1,5 +1,11 @@
 // JavaScript Document
 
 $(document).ready(function () {
-
+	$.get('page-1.html', function (data) {
+	$('body').append(data);
+	});
+	
+	$('.current').on('click', function() {
+		$('.current').load('page-3.html');
+	});
 });

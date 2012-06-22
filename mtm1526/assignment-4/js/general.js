6dfe62cb817a7737b328a052fa318514ae671707
Scott Grimes <sgrimes@rogers.com>
2012-06-22 01:53:28 -0400

    tabs now changing with css

--- mtm1526/assignment-4/js/general.js
+++ mtm1526/assignment-4/js/general.js
@@ -2,30 +2,35 @@
 
 $(document).ready(function () {
 	$.get('page-1.html', function (data) {
-	$('#load-content').append(data);
+	$('#load-content').prepend(data);
 	});
 	
 $('#tab-1').on('click', function(ev) {
 		ev.preventDefault();
-
+		$("li").removeClass("current-tab");
+		$('#tab-1').addClass("current-tab");
 		$('#load-content').load('page-1.html');
+	
 	});
 
 $('#tab-2').on('click', function(ev) {
 		ev.preventDefault();
-
+		$("li").removeClass("current-tab");
+		$('#tab-2').addClass("current-tab");
 		$('#load-content').load('page-2.html');
 	});
 	
 $('#tab-3').on('click', function(ev) {
 		ev.preventDefault();
-
+		$("li").removeClass("current-tab");
+		$('#tab-3').addClass("current-tab");
 		$('#load-content').load('page-3.html');
 	});
 	
 $('#tab-4').on('click', function(ev) {
 		ev.preventDefault();
-
+		$("li").removeClass("current-tab");
+		$('#tab-4').addClass("current-tab");
 		$('#load-content').load('page-4.html');
 	});
 	
